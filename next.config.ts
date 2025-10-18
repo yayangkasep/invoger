369bf8318b3b88ce05cacf9b9a04a2ad5f9b0e1b
Jardani Jovonovich <33832706+yayangkasep@users.noreply.github.com>
2025-10-19 01:54:09 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    // during development, don't block on ESLint errors
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // during development, don't block on TS errors
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
